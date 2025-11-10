0f1ceb8abdd8b4093420b167298c4a3269210287
ardhiancalwa <ardhiancalwaofc@gmail.com>
2025-11-11 02:30:13 +0700

    feat: add authentication, user CRUD, repository, and service layer

---
+++ app/Services/UserService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+
+class UserService
+{
+    protected $users;
+
+    public function __construct(UserRepository $users)
+    {
+        $this->users = $users;
+    }
+
+    public function getAll()
+    {
+        return $this->users->all();
+    }
+
+    public function getById($id)
+    {
+        return $this->users->find($id);
+    }
+
+    public function create(array $data)
+    {
+        $data['password'] = bcrypt($data['password']);
+        return $this->users->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        if (isset($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+        return $this->users->update($id, $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->users->delete($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,35 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+    });
+});
+
+Route::prefix('v1')->middleware('auth:api')->group(function () {
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/users', [UserController::class, 'index']);
+        Route::get('/users/{id}', [UserController::class, 'show']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::put('/users/{id}/update', [UserController::class, 'update']);
+        Route::delete('/users/{id}/delete', [UserController::class, 'destroy']);
+    });
+
+});
+
+Route::fallback(function () {
+    return response()->json([
+        'success' => false,
+        'message' => 'API endpoint not found'
+    ], 404);
+});
