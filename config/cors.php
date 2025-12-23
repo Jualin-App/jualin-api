abd78c5767381eb4ec99a4928e5ccfa69bd37a9f
ardhiancalwa <ardhiancalwaofc@gmail.com>
2025-12-24 02:58:47 +0700

    chore: update cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'v1/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://192.168.0.101:3000,https://helaine-cleanlier-noncalculably.ngrok-free.dev')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://192.168.0.101:3000,https://helaine-cleanlier-noncalculably.ngrok-free.dev,https://www.jualin-tel.biz.id')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
